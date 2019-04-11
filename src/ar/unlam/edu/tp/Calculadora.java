7204b5fbdcd6963494cbe4e61f4c4a56e3a6dcb3
Nicolás <nicomarmiroli@gmail.com>
2019-04-11 00:27:53 -0300

    Primer Commit con Switch

---
+++ src/ar/unlam/edu/tp/Calculadora.java
@@ -0,0 +1,38 @@
+package ar.unlam.edu.tp;
+
+import java.util.Scanner;
+
+public class Calculadora {
+	public static void main(String[] args) {
+		Integer a;
+		Integer b;
+		Integer opcion;
+		Integer resultado;
+ do{
+ System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+ opcion = teclado.next();
+ } 
+ while(opcion < 1 && opcion > 4)
+ System.out.println("Ingrese el primer numero");
+ a = teclado.next();
+ System.out.println("Ingrese el segundo numero");
+ a = teclado.next();
+ 
+ switch(opcion) {
+ case 1:
+	 resultado = a + b;
+	 break;
+ case 2:
+	 resultado = a - b;
+	 break;
+ case 3:
+	 resultado = a * b;
+	 break;
+ case 4:
+	 resultado = a / b;
+	 break;
+ } 
+ 
+ System.out.println("El resultado es " + resultado.toString());
+ }
+}
