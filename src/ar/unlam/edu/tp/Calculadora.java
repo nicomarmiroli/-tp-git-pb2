1d575f28ca4355284d284a55c911591e038b78f0
ferseba18 <fer_seba_18@yahoo.com.ar>
2019-04-12 16:27:18 -0300

    Update Calculadora.java

--- src/ar/unlam/edu/tp/Calculadora.java
+++ src/ar/unlam/edu/tp/Calculadora.java
@@ -9,7 +9,7 @@ public class Calculadora {
 		Integer opcion;
 		Integer resultado;
  do{
- System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+ System.out.println("Ingrese su operación /n 1 para multiplicar /n 2 para dividir /n 3 para sumar /n 4 para restar");
  opcion = teclado.next();
  } 
  while(opcion < 1 && opcion > 4)
@@ -20,16 +20,16 @@ public class Calculadora {
  
  switch(opcion) {
  case 1:
-	 resultado = a + b;
-	 break;
- case 2:
-	 resultado = a - b;
-	 break;
- case 3:
 	 resultado = a * b;
 	 break;
- case 4:
+ case 2:
 	 resultado = a / b;
+	 break;
+ case 3:
+	 resultado = a + b;
+	 break;
+ case 4:
+	 resultado = a - b;
 	 break;
  } 
  
