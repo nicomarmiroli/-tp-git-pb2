41c7567aa109e575e39628871c58c230319526b2
ferseba18 <fer_seba_18@yahoo.com.ar>
2019-04-16 15:50:02 -0300

    Add files via upload

--- src/ar/unlam/edu/tp/Calculadora.java
+++ src/ar/unlam/edu/tp/Calculadora.java
@@ -9,13 +9,13 @@ public class Calculadora {
 		Integer opcion;
 		Integer resultado;
  do{
- System.out.println("Ingrese su operación /n 1 para multiplicar /n 2 para dividir /n 3 para sumar /n 4 para restar");
+ System.out.println("Ingrese su operación /n 1 para multiplicar /n 2 para dividir /n 3 para sumar /n 4 para restar  /n 5 para potenciar");
  opcion = teclado.next();
  } 
- while(opcion < 1 && opcion > 4)
- System.out.println("Ingrese el primer numero");
+ while(opcion < 1 && opcion > 5)
+ System.out.println("Ingrese el primer numero(o la base si es potencia");
  a = teclado.next();
- System.out.println("Ingrese el segundo numero");
+ System.out.println("Ingrese el segundo numero(o el exponente si es potencia");
  a = teclado.next();
  
  switch(opcion) {
@@ -31,6 +31,8 @@ public class Calculadora {
  case 4:
 	 resultado = a - b;
 	 break;
+ case 5:
+	 resultado = (int) Math.pow(a,b);
  } 
  
  System.out.println("El resultado es " + resultado.toString());
