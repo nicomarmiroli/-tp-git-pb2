03b7bdadcbbbc017f0b2c176c5f606af9548b8ff
ferseba18 <fer_seba_18@yahoo.com.ar>
2019-04-16 16:15:07 -0300

    Update Calculadora.java

--- src/ar/unlam/edu/tp/Calculadora.java
+++ src/ar/unlam/edu/tp/Calculadora.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class Calculadora {
 	public static void main(String[] args) {
-		Integer a;
-		Integer b;
+		Float a;
+		Float b;
 		Integer opcion;
-		Integer resultado;
+		Float resultado;
  do{
- System.out.println("Ingrese su operación /n 1 para multiplicar /n 2 para dividir /n 3 para sumar /n 4 para restar");
+ System.out.println("Ingrese su operaciÃ³n /n 1 para multiplicar /n 2 para dividir /n 3 para sumar /n 4 para restar  /n 5 para radicar");
  opcion = teclado.next();
  } 
- while(opcion < 1 && opcion > 4)
- System.out.println("Ingrese el primer numero");
+ while(opcion < 1 && opcion > 5)
+ System.out.println("Ingrese el primer numero(o el radicando si es radicacion");
  a = teclado.next();
- System.out.println("Ingrese el segundo numero");
+ System.out.println("Ingrese el segundo numero(o el indice si es radicacion");
  a = teclado.next();
  
  switch(opcion) {
@@ -31,6 +31,8 @@ public class Calculadora {
  case 4:
 	 resultado = a - b;
 	 break;
+ case 5:
+	 resultado = (float) Math.pow(a,1/b);
  } 
  
  System.out.println("El resultado es " + resultado.toString());
