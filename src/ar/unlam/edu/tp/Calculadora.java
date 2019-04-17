16b069d6122725d40779cf69d0a4fbdbcb15537d
cosentinowebdev <48664024+cosentinowebdev@users.noreply.github.com>
2019-04-16 22:27:56 -0300

    Update Calculadora.java

--- src/ar/unlam/edu/tp/Calculadora.java
+++ src/ar/unlam/edu/tp/Calculadora.java
@@ -13,7 +13,7 @@ public class Calculadora {
 				 opcion = teclado.next();
 			 } 
 			 while(opcion < 1 && opcion > 4)
-			 	
+
 				System.out.println("Ingrese el primer numero");
 				a = teclado.next();
 				System.out.println("Ingrese el segundo numero");
@@ -21,18 +21,18 @@ public class Calculadora {
 			 
 			 switch(opcion) 
 			 {
-				 case 1:
-					 resultado = a * b;
-					 break;
-				 case 2:
-					 resultado = a / b;
-					 break;
-				 case 3:
-					 resultado = a + b;
-					 break;
-				 case 4:
-					 resultado = a - b;
-					 break;
+				case 1:
+					resultado = a * b;
+					break;
+				case 2:
+					resultado = a / b;
+					break;
+				case 3:
+				 	resultado = a - b;
+					break;
+				case 4:
+					resultado = a + b;
+					break;
 			 } 
 			 
 			 System.out.println("El resultado es " + resultado.toString());
