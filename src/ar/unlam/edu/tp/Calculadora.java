bbaeaf772c82c6b85c146a33decfa44ddd22b361
cosentinowebdev <48664024+cosentinowebdev@users.noreply.github.com>
2019-04-16 22:26:15 -0300

    Update Calculadora.java

--- src/ar/unlam/edu/tp/Calculadora.java
+++ src/ar/unlam/edu/tp/Calculadora.java
@@ -8,33 +8,33 @@ public class Calculadora {
 		Integer b;
 		Integer opcion;
 		Integer resultado;
- do{
-	 System.out.println("Ingrese su operación /n 1 para multiplicar /n 2 para dividir /n 3 para sumar /n 4 para restar");
-	 opcion = teclado.next();
- } 
- while(opcion < 1 && opcion > 4)
- 	
-	System.out.println("Ingrese el primer numero");
-	a = teclado.next();
-	System.out.println("Ingrese el segundo numero");
-	a = teclado.next();
- 
- switch(opcion) 
- {
-	 case 1:
-		 resultado = a * b;
-		 break;
-	 case 2:
-		 resultado = a / b;
-		 break;
-	 case 3:
-		 resultado = a + b;
-		 break;
-	 case 4:
-		 resultado = a - b;
-		 break;
- } 
- 
- System.out.println("El resultado es " + resultado.toString());
+			 do{
+				 System.out.println("Ingrese su operación /n 1 para multiplicar /n 2 para dividir /n 3 para sumar /n 4 para restar");
+				 opcion = teclado.next();
+			 } 
+			 while(opcion < 1 && opcion > 4)
+			 	
+				System.out.println("Ingrese el primer numero");
+				a = teclado.next();
+				System.out.println("Ingrese el segundo numero");
+				a = teclado.next();
+			 
+			 switch(opcion) 
+			 {
+				 case 1:
+					 resultado = a * b;
+					 break;
+				 case 2:
+					 resultado = a / b;
+					 break;
+				 case 3:
+					 resultado = a + b;
+					 break;
+				 case 4:
+					 resultado = a - b;
+					 break;
+			 } 
+			 
+			 System.out.println("El resultado es " + resultado.toString());
  }
 }
